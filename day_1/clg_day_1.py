6c81f4b4dcac4bd120201103d416459d1599b7af
hwyden <me9asety4@gmail.com>
2023-12-12 20:30:37 +0700

    challenge day 1

---
+++ day_1/clg_day_1.py
@@ -0,0 +1,24 @@
+# DESCRIPTION:
+'''
+Given an array of integers your solution should find the smallest integer.
+
+For example:
+
+Given [34, 15, 88, 2] your solution will return 2
+Given [34, -345, -1, 100] your solution will return -345
+You can assume, for the purpose of this kata, that the supplied array will not be empty.
+'''
+
+# Solutions
+def find_smallest_int(arr):
+    min_arr = min(arr)
+    return min_arr
+
+
+
+
+
+
+'''
+Ainun done this first, so I challenge this with another one same as Ainun
+'''
