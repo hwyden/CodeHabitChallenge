fa8e9c0d219f4caf7707b4e4533731673c1c6277
hwyden <me9asety4@gmail.com>
2023-12-13 19:17:36 +0700

    challenge day 2

---
+++ day_2/solution_day2.py
@@ -0,0 +1,29 @@
+# DESCRIPTION:
+'''
+Create a function close_compare that accepts 3 parameters: a, b, and an optional margin. The function should return whether a is lower than, close to, or higher than b.
+
+Please note the following:
+
+When a is close to b, return 0.
+For this challenge, a is considered "close to" b if margin is greater than or equal to the absolute distance between a and b.
+Otherwise...
+
+When a is less than b, return -1.
+
+When a is greater than b, return 1.
+
+If margin is not given, treat it as if it were zero.
+
+Assume: margin >= 0
+
+Tip: Some languages have a way to make parameters optional.
+'''
+
+# Solution
+def close_compare(a, b, margin=0):
+    if abs(a - b) <= margin:
+        return 0
+    elif a < b:
+        return -1
+    else:
+        return 1
